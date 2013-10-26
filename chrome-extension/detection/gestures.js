5169c2cf127786e26527ab664e5aaae6a8029e40
Michael Kay <kay.mikey@gmail.com>
2013-10-26 14:51:38 +0100

    Mike/Alina #extension renamed template for extension; added API

---
+++ chrome-extension/detection/gestures.js
@@ -0,0 +1,25 @@
+function gestures() {
+
+    function left(callback) {
+
+    }
+
+    function right(callback) {
+
+    }
+
+    function up(callback) {
+
+    }
+
+    function down(callback) {
+
+    }
+
+    return {
+        left: left,
+        right: right,
+        up: up,
+        down: down
+    };
+}
