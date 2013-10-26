73d44b9e0f2e0fb305c4b16d9a506e40a3d3721b
Michael Kay <kay.mikey@gmail.com>
2013-10-26 15:06:42 +0100

    Mike/Alina #extension Gestures added

--- chrome-extension/popup/extension.js
+++ chrome-extension/popup/extension.js
@@ -1,10 +1,30 @@
-function extension() {
+function extension(gestures) {
 
     function init() {
+        gestures.up(function() {
 
+        });
+
+        gestures.down(function() {
+
+        });
+
+        gestures.left(function() {
+
+        });
+
+        gestures.right(function() {
+
+        });
     }
 
     return {
         init: init
     };
 }
+
+$(function(){
+    var gestures = gestures();
+
+    extension(gestures).init();
+});
