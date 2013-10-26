9f51d971fdc33065d769e1272909cbcdcce862dd
Michael Kay <kay.mikey@gmail.com>
2013-10-26 15:18:46 +0100

    Mike/Alina #extension Added scroll attempt

--- chrome-extension/popup/extension.js
+++ chrome-extension/popup/extension.js
@@ -2,11 +2,11 @@ function extension(gestures) {
 
     function init() {
         gestures.up(function() {
-
+            $('html, body').animate({ scrollTop: $(document).offset().top }, 0);
         });
 
         gestures.down(function() {
-
+            $('html, body').animate({ scrollTop: $(document).height() }, 1000);
         });
 
         gestures.left(function() {
