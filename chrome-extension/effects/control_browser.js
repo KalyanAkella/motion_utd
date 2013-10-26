593d7b8db5258d5a65cf407b4805bad45413971b
Michael Kay <kay.mikey@gmail.com>
2013-10-26 22:02:52 +0100

    K... last refactor for now

--- chrome-extension/effects/control_browser.js
+++ chrome-extension/effects/control_browser.js
@@ -2,34 +2,34 @@ function control_browser() {
 
     var SPEED = 500;
 
-    function scroll_left() {
+    function left() {
         $('html, body').animate({
             scrollLeft: '-=300'
         }, SPEED);
     }
 
-    function scroll_right() {
+    function right() {
         $('html, body').animate({
             scrollLeft: '+=300'
         }, SPEED);
     }
 
-    function scroll_up() {
+    function up() {
         $('html, body').animate({
             scrollTop: '-=300'
         }, SPEED);
     }
 
-    function scroll_down() {
+    function down() {
         $('html, body').animate({
             scrollTop: '+=300'
         }, SPEED);
     }
 
     return {
-        scroll_left: scroll_left,
-        scroll_right: scroll_right,
-        scroll_up: scroll_up,
-        scroll_down: scroll_down
+        left: left,
+        right: right,
+        up: up,
+        down: down
     }
 }
--- chrome-extension/popup/broker.js
+++ chrome-extension/popup/broker.js
@@ -3,10 +3,10 @@ function broker(browser) {
     function add_publisher(publisher){
         publisher.init();
 
-        publisher.up(browser.scroll_up);
-        publisher.down(browser.scroll_down);
-        publisher.left(browser.scroll_left);
-        publisher.right(browser.scroll_right);
+        publisher.up(browser.up);
+        publisher.down(browser.down);
+        publisher.left(browser.left);
+        publisher.right(browser.right);
     }
 
     return {
