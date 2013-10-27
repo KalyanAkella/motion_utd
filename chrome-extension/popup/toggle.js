d6e28ed94dd144575caa06ea19eabe2115d57444
Bamdad Dashtban <bamdadd@gmail.com>
2013-10-27 02:08:38 +0000

    Mike,Bamdad fixing the show/hide icon

--- chrome-extension/popup/toggle.js
+++ chrome-extension/popup/toggle.js
@@ -1,7 +1,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript(null, {
     code: '$("#motion-example-gestures").toggle();' +
-          '$("#motion-gestures").toggle();'
+          '$("#motion-gestures-ver").toggle();'+
+          '$("#motion-gestures-hor").toggle();'
   });
 });
 
